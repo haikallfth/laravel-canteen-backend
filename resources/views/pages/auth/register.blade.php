d05e0a0dc088df50cb619237c77fe253a29ff507
ichall <haikalalfatih100208@gmail.com>
2024-10-21 09:23:32 +0700

    feat: auth register

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -14,40 +14,49 @@
     </div>
 
     <div class="card-body">
-        <form method="POST">
-
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
             <div class="form-group">
-                <label for="frist_name">Name</label>
-                <input id="frist_name" type="text" class="form-control" name="frist_name" autofocus>
+                <label for="name">Name</label>
+                <input id="name" value="{{ old('name') }}" type="text" class="form-control" name="name" autofocus>
             </div>
 
             <div class="form-group">
                 <label for="email">Email</label>
-                <input id="email" type="email" class="form-control" name="email">
+                <input id="email" value="{{ old('email') }}" type="email" class="form-control" name="email">
                 <div class="invalid-feedback">
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="password" class="d-block">Password</label>
-                <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator"
-                    name="password">
+                <input id="password" type="password" class="form-control pwstrength @error('password') is-invalid @enderror" data-indicator="pwindicator" name="password">
+                @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
                 <div id="pwindicator" class="pwindicator">
                     <div class="bar"></div>
                     <div class="label"></div>
                 </div>
             </div>
-            
+
             <div class="form-group">
                 <label for="password2" class="d-block">Password Confirmation</label>
-                <input id="password2" type="password" class="form-control" name="password-confirm">
+                <input id="password2" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation">
+                @error('password_confirmation')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-lg btn-block">
                     Register
                 </button>
-            </div>
+            </div>          
         </form>
     </div>
 </div>
